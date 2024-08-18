7c7b13d86397df7a4283ca322dd7bd8be396cc29
Bruno Alehtxandre <brunoaleh1@gmail.com>
2024-08-18 15:50:14 -0300

    estruturando o usecase de car e carproduct

--- repository/car.go
+++ repository/car.go
@@ -71,18 +71,28 @@ func (cr *CarRepository) GetCarByID(id int) (model.Car, error) {
 	return carObj, nil
 }
 
-func (cr *CarRepository) GetCarByUserID(userID int) (model.Car, error) {
+func (cr *CarRepository) GetCarsByUserID(userID int) ([]model.Car, error) {
 	query := "SELECT id, user_id, status, created_at, updated_at FROM cars WHERE user_id = $1"
-	row := cr.connection.QueryRow(query, userID)
-
-	var carObj model.Car
-	err := row.Scan(&carObj.ID, &carObj.UserID, &carObj.Status, &carObj.CreatedAt, &carObj.UpdatedAt)
+	rows, err := cr.connection.Query(query, userID)
 	if err != nil {
-		log.Printf("Error scanning car: %v", err)
-		return model.Car{}, err
+		log.Println(err)
+		return []model.Car{}, err
+	}
+	defer rows.Close()
+
+	var carList []model.Car
+	var carObj model.Car
+
+	for rows.Next() {
+		err := rows.Scan(&carObj.ID, &carObj.UserID, &carObj.Status, &carObj.CreatedAt, &carObj.UpdatedAt)
+		if err != nil {
+			log.Println(err)
+			return []model.Car{}, err
+		}
+		carList = append(carList, carObj)
 	}
 
-	return carObj, nil
+	return carList, nil
 }
 
 func (cr *CarRepository) UpdateCarStatus(carID int, status string) (int, error) {
--- usecase/car_usecase.go
+++ usecase/car_usecase.go
@@ -1,6 +1,27 @@
 package usecase
 
-import "go-api-commerce/repository"
+import (
+	"go-api-commerce/model"
+	"go-api-commerce/repository"
+)
+
+type IResponseCars struct {
+	Message string      `json:"message"`
+	Data    []model.Car `json:"data"`
+	Success bool        `json:"success"`
+}
+
+type IResponseCarProducts struct {
+	Message string             `json:"message"`
+	Data    []model.CarProduct `json:"data"`
+	Success bool               `json:"success"`
+}
+
+type IResponseCar struct {
+	Message string    `json:"message"`
+	Data    model.Car `json:"data"`
+	Success bool      `json:"success"`
+}
 
 type CarUseCase struct {
 	cr  repository.CarRepository
@@ -11,5 +32,254 @@ func NewCarUseCase(cr repository.CarRepository, cpr repository.CarProductReposit
 	return &CarUseCase{
 		cr:  cr,
 		cpr: cpr,
+	}
+}
+
+func (uc *CarUseCase) GetCars() IResponseCars {
+	cars, err := uc.cr.GetCars()
+	if err != nil {
+		return IResponseCars{
+			Message: "Error getting cars" + err.Error(),
+			Data:    []model.Car{},
+			Success: false,
+		}
+	}
+
+	return IResponseCars{
+		Message: "Success getting cars",
+		Data:    cars,
+		Success: true,
+	}
+}
+
+func (uc *CarUseCase) GetCarsByUserID(userID int) IResponseCars {
+	cars, err := uc.cr.GetCarsByUserID(userID)
+	if err != nil {
+		return IResponseCars{
+			Message: "Error getting cars" + err.Error(),
+			Data:    []model.Car{},
+			Success: false,
+		}
+	}
+
+	return IResponseCars{
+		Message: "Success getting cars",
+		Data:    cars,
+		Success: true,
+	}
+}
+
+func (uc *CarUseCase) GetCarByID(id int) IResponseCar {
+	car, err := uc.cr.GetCarByID(id)
+	if err != nil {
+		return IResponseCar{
+			Message: "Error getting car" + err.Error(),
+			Data:    model.Car{},
+			Success: false,
+		}
+	}
+
+	return IResponseCar{
+		Message: "Success getting car",
+		Data:    car,
+		Success: true,
+	}
+}
+
+func (uc *CarUseCase) CreateCar(car model.Car) IResponseCar {
+	id, err := uc.cr.CreateCar(car)
+	if err != nil {
+		return IResponseCar{
+			Message: "Error creating car" + err.Error(),
+			Data:    model.Car{},
+			Success: false,
+		}
+	}
+
+	car.ID = id
+	return IResponseCar{
+		Message: "Success creating car",
+		Data:    car,
+		Success: true,
+	}
+}
+
+func (uc *CarUseCase) CreateCarWithProducts(car model.Car, carProducts []model.CarProduct) IResponseCarProducts {
+	id, err := uc.cr.CreateCar(car)
+	if err != nil {
+		return IResponseCarProducts{
+			Message: "Error creating car" + err.Error(),
+			Data:    []model.CarProduct{},
+			Success: false,
+		}
+	}
+
+	car.ID = id
+
+	for i := 0; i < len(carProducts); i++ {
+		carProducts[i].CarID = car.ID
+		_, err := uc.cpr.CreateCarProduct(carProducts[i])
+		if err != nil {
+			return IResponseCarProducts{
+				Message: "Error creating car product" + err.Error(),
+				Data:    []model.CarProduct{},
+				Success: false,
+			}
+		}
+	}
+
+	listCarProduct, err := uc.cpr.GetCarProductsByCarID(car.ID)
+	if err != nil {
+		return IResponseCarProducts{
+			Message: "Error getting product in car" + err.Error(),
+			Data:    []model.CarProduct{},
+			Success: false,
+		}
+	}
+
+	return IResponseCarProducts{
+		Message: "Success creating car",
+		Data:    listCarProduct,
+		Success: true,
+	}
+}
+
+func (uc *CarUseCase) AddProductToCar(carProduct model.CarProduct) IResponseCarProducts {
+	id, err := uc.cpr.CreateCarProduct(carProduct)
+	if err != nil {
+		return IResponseCarProducts{
+			Message: "Error adding product to car" + err.Error(),
+			Data:    []model.CarProduct{},
+			Success: false,
+		}
+	}
+
+	carProduct.ID = id
+
+	listCarProduct, err := uc.cpr.GetCarProductsByCarID(carProduct.CarID)
+	if err != nil {
+		return IResponseCarProducts{
+			Message: "Error getting product in car" + err.Error(),
+			Data:    []model.CarProduct{},
+			Success: false,
+		}
+	}
+
+	return IResponseCarProducts{
+		Message: "Success adding product to car",
+		Data:    listCarProduct,
+		Success: true,
+	}
+}
+
+func (uc *CarUseCase) UpdateCarStatus(carID int, status string) IResponseCar {
+	_, err := uc.cr.UpdateCarStatus(carID, status)
+	if err != nil {
+		return IResponseCar{
+			Message: "Error updating car status" + err.Error(),
+			Data:    model.Car{},
+			Success: false,
+		}
+	}
+
+	car, err := uc.cr.GetCarByID(carID)
+	if err != nil {
+		return IResponseCar{
+			Message: "Error getting car" + err.Error(),
+			Data:    model.Car{},
+			Success: false,
+		}
+	}
+
+	return IResponseCar{
+		Message: "Success updating car status",
+		Data:    car,
+		Success: true,
+	}
+}
+
+func (uc *CarUseCase) UpdateProductQuantity(userID, carID int, carProduct model.CarProduct) IResponseCarProducts {
+	car, err := uc.cr.GetCarByID(carID)
+	if err != nil {
+		return IResponseCarProducts{
+			Message: "Error getting car" + err.Error(),
+			Data:    []model.CarProduct{},
+			Success: false,
+		}
+	}
+
+	if car.UserID != userID {
+		return IResponseCarProducts{
+			Message: "Error getting not user's car",
+			Data:    []model.CarProduct{},
+			Success: false,
+		}
+	}
+
+	_, err = uc.cpr.UpdateCarProduct(carProduct)
+	if err != nil {
+		return IResponseCarProducts{
+			Message: "Error updating product quantity" + err.Error(),
+			Data:    []model.CarProduct{},
+			Success: false,
+		}
+	}
+
+	listCarProduct, err := uc.cpr.GetCarProductsByCarID(carProduct.CarID)
+	if err != nil {
+		return IResponseCarProducts{
+			Message: "Error getting product in car" + err.Error(),
+			Data:    []model.CarProduct{},
+			Success: false,
+		}
+	}
+
+	return IResponseCarProducts{
+		Message: "Success updating product quantity",
+		Data:    listCarProduct,
+		Success: true,
+	}
+}
+
+func (uc *CarUseCase) RemoveProductFromCar(userID, carID, productId int) IResponseCarProducts {
+
+	car, err := uc.cr.GetCarByID(carID)
+	if err != nil {
+		return IResponseCarProducts{
+			Message: "Error getting car" + err.Error(),
+			Data:    []model.CarProduct{},
+			Success: false,
+		}
+	}
+	if car.UserID != userID {
+		return IResponseCarProducts{
+			Message: "Error getting not user's car",
+			Data:    []model.CarProduct{},
+			Success: false,
+		}
+	}
+
+	_, err = uc.cpr.DeleteCarProduct(productId)
+	if err != nil {
+		return IResponseCarProducts{
+			Message: "Error removing product from car" + err.Error(),
+			Data:    []model.CarProduct{},
+			Success: false,
+		}
+	}
+
+	listCarProduct, err := uc.cpr.GetCarProductsByCarID(carID)
+	if err != nil {
+		return IResponseCarProducts{
+			Message: "Error getting product in car" + err.Error(),
+			Data:    []model.CarProduct{},
+			Success: false,
+		}
+	}
+
+	return IResponseCarProducts{
+		Message: "Success removing product from car",
+		Data:    listCarProduct,
+		Success: true,
 	}
 }
